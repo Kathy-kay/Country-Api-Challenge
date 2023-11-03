c1ce69911393fbe7ec11f522bffef4301e8ad7b0
Kathy-kay <adigunkafy27@gmail.com>
2023-11-03 20:19:59 +0100

    discarded unused component

--- src/components/countryapi.js
+++ src/components/countryapi.js
@@ -17,3 +17,15 @@ export const useRegion = (region) => {
     return response.data.slice(0, 20);
   });
 };
+
+export const useCountrySearch = (name) => {
+  return (
+    useQuery(["countrySearch", name]),
+    async () => {
+      const response = await axios.get(
+        `https://restcountries.com/v3.1/${name}`
+      );
+      return response.data;
+    }
+  );
+};
--- src/components/customHook.js
+++
@@ -1,5 +0,0 @@
-import { useRegion } from "./countryapi";
-
-export const useCustomCountryRegion = (region) => {
-  return useRegion(region);
-};
